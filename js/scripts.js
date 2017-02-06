1cee48feb95dfc55bb8a9d70dc25cdd58f30300c
Ania <ania@MacBook-Pro-Anna.local>
2017-02-06 02:00:25 +0100

    area of a triangle

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// plik scripts.js
+
+var a = 1,
+	h = 1,
+	triangleArea = 0;
+a = prompt('Wpisz długość podstawy trójkąta');
+h = prompt('Wpisz wysokość trójkąta');
+triangleArea = a * h / 2;
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+alert('Pole trójkąta wynosi: ' + triangleArea);
